86eaa41a4c0ce552e26ece9b46fec790072a42d2
Liara42 <caroline1481@gmail.com>
2020-11-15 17:56:12 +0100

    Load Message Added

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,10 +30,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(
         'https://first-vue-http-task.firebaseio.com/surveys.json'
         // method: 'GET',  //1. It is default, so it's not needed
@@ -55,6 +58,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
